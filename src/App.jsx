8ddee3f4c935ee7daf75b606322e2e5b48339685
AlishaDarly-mountbox <alisha.d@mountbox.in>
2026-01-09 12:28:10 +0530

    Fix routing: Switch to HashRouter to resolve SPA routing issues on Render

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import HeroSlider from './components/HeroSlider'
 import HeroFilter from './components/HeroFilter'
